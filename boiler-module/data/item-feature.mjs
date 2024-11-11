28f0695b3f1498fcfc86e440c1aa8a039d958ba1
pwatson100 <paul@skynet2020.co.uk>
2024-11-11 10:13:26 +0000

    some modules

--- boiler-module/data/item-feature.mjs
+++
@@ -1,8 +0,0 @@
-import totowItemBase from './base-item.mjs';
-
-export default class totowFeature extends totowItemBase {
-  static LOCALIZATION_PREFIXES = [
-    'TALESOFTHEOLDWEST.Item.base',
-    'TALESOFTHEOLDWEST.Item.Feature',
-  ];
-}
